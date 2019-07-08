a601b9c54e739b6d559b60e0ede213071dbe7240
Kenneth Eriksson <eriksson.r.k@gmail.com>
2019-07-08 14:13:52 +0200

    Added a GlobalStyle, Added css reset, added custom global style, Added font to index.html

--- src/Hangman.js
+++ src/Hangman.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './ui/Header'
 import { Game } from './Game'
-
+import GlobalStyle from './Global'
 
 const Hangman = () => {
 	return (
 		<Game>
+			<GlobalStyle />
 			<Header />
+			<div>Testing-div</div>
 		</Game>
 	)
 }
